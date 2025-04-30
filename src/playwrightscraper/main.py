3ddc20ac507b0b7520af5158a3a4cee8e0abb99e
Sai <ysaijagadeeshuh@gmail.com>
2025-04-30 09:40:58 -0500

    no red deletions

--- src/playwrightscraper/main.py
+++ src/playwrightscraper/main.py
@@ -1,17 +1,12 @@
-from taipy.gui import Gui, State, notify, Markdown
+from taipy.gui import Gui, Markdown
 import taipy.gui.builder as tgb
-from datetime import datetime, date, timedelta
-from pathlib import Path
 from utils import getStats
 from botConfig import PATH_LIST
 from pages import StatusChecker, PointCapPage, SegmentCapPage, StorageCapPage, NoNoticeActivityPage, DataPreview
 from azurepush import pushFiles
 # from flask import Flask
-import gevent.pywsgi
 
 df = None
-
-# flask_app = Flask(__name__)
 
 
 def on_navigate(state, page_name: str):
@@ -20,24 +15,9 @@ def on_navigate(state, page_name: str):
         with tgb.Page() as statustable:
             with tgb.part(class_name="card"):
                 tgb.table(data="{df}", filter=True, sortable=True)
-        # pagecontent = Markdown("<|{df}|table|>")
         state.df_status.update_content(state, statustable)
         pushFiles()
         return "status"
-    # if page_name == "preview":
-    #     with open(PATH_LIST["TEMP_TXT"], 'w+') as file:
-    #         filename = file.read()
-    #     print(filename)
-    #     state.df = processFiles(Path(filename))
-    #     with tgb.Page() as previewtable:
-    #         with tgb.part(class_name="card"):
-    #             tgb.table(data="{df}", filter=True, sortable=True)
-    #     # pagecontent = Markdown("<|{df}|table|>")
-    #     state.df_preview.update_content(state, previewtable)
-    #     pushFiles()
-    #     return "preview"
-    # else:
-    #     print(state.SubmitActive, page_name)
     return page_name
 
 
@@ -49,12 +29,10 @@ with tgb.Page() as homepage:
                     ("/segmentcap", "Segment Capacity"),
                     ("/storagecap", "Storage Capacity"),
                     ("/nonoticeactivity", "No Notice Activity"),
-                    ("/status", "Runs Status"),
-                    # ("/preview", "Preview")
+                    ("/status", "Runs Status")
                     ])
 
     tgb.html("br")
-
 
 pagelist = {
     "/": homepage,
@@ -62,20 +40,18 @@ pagelist = {
     "segmentcap": SegmentCapPage,
     "storagecap": StorageCapPage,
     "nonoticeactivity": NoNoticeActivityPage,
-    "status": StatusChecker,
-    # "preview": DataPreview
-    # "/status": None
+    "status": StatusChecker
 }
-# #21274A
+
+# app = Flask(__name__)
+
 gui = Gui(pages=pagelist)
 df_status = gui.add_partial(Markdown("<|{df}|table|>"))
 
-gui_app = gui.run(title="GasFundies", debug=False,
-                  port="8000", allow_unsafe_werkzeug=True, run_server=False)
 
-
-app_server = gevent.pywsgi.WSGIServer(('127.0.0.1', 8088), gui_app)
-app_server.serve_forever()
-# # if __name__ == "__main__":
 # gui.run(title="GasFundies", debug=False,
-#         port="8000", allow_unsafe_werkzeug=True)
+#         allow_unsafe_werkzeug=True, run_server=False)
+
+if __name__ == '__main__':
+    # app.run(debug=False, threaded=True,  port=5000)
+    gui.run(title="GasFundies", debug=False, port=5000)
--- src/playwrightscraper/runner.py
+++
@@ -1,5 +0,0 @@
-from .main import gui_app
-import gevent.pywsgi
-
-app_server = gevent.pywsgi.WSGIServer(('127.0.0.1', 8088), gui_app)
-app_server.serve_forever()
